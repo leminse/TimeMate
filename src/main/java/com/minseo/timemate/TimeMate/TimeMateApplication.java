c0fe1a072a8fc2958d2236dc98d1ffa3cc28bcc5
minseo <minseo@DESKTOP-NL04HGM>
2025-07-02 16:09:48 +0900

    로그인 (프론트)

---
+++ src/main/java/com/minseo/timemate/TimeMate/TimeMateApplication.java
@@ -0,0 +1,13 @@
+package com.minseo.timemate.TimeMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minseo/timemate/TimeMate/TimeMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minseo.timemate.TimeMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
